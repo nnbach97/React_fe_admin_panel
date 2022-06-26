55915155bb47409aee35f3810dd074b432fdbd9d
bach.ngongoc <nnbach1997@gmail.com>
2022-06-26 21:34:19 +0700

    ADD source + base

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./scss/App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Blank from "./pages/Blank";
+import MainLayout from "./layout/MainLayout";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="orders" element={<Blank />} />
+            <Route path="products" element={<Blank />} />
+            <Route path="customers" element={<Blank />} />
+            <Route path="stats" element={<Blank />} />
+            <Route path="settings" element={<Blank />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+import Sidebar from '../components/sidebar/Sidebar'
+import Topnav from '../components/topnav/Topnav'
+// import './main-layout.scss'
+
+const MainLayout = () => {
+  return (
+    <>
+      <Sidebar/>
+      <div className='main'>
+        <div className="main__content">
+          <Topnav/>
+          <Outlet/>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default MainLayout
